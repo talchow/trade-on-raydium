f39a8f829a86a3baf838bd57502b46bc6caaea0e
talchow <talchow.chow6@gmail.com>
2025-06-13 18:45:10 +0800

    update the dependence of raydium_swap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::{env, sync::Arc};
+use ftlog::trace;
 use raydium_swap::{amm::executor::*, api_v3::ApiV3Client, types::{SwapExecutionMode, SwapInput}};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{pubkey::Pubkey, signer::Signer, transaction::VersionedTransaction};
 use solana_sdk::pubkey;
+
 const SOL: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
 const USDC: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
 #[tokio::main]
 pub async fn main() ->anyhow::Result<()> {
+    let _guard = ftlog::builder().try_init().unwrap();
     let client = Arc::new(RpcClient::new("http://api.testnet.solana.com".to_string()));
     let executor = RaydiumAmm::new(
         Arc::clone(&client),
         RaydiumAmmExecutorOpts::default(),
         ApiV3Client::new(None)
     );
+    trace!("Creating executor sucess!");
 
     let swap_input = SwapInput {
         input_token_mint: SOL,
@@ -22,24 +26,24 @@ pub async fn main() ->anyhow::Result<()> {
         mode: SwapExecutionMode::ExactIn,
         market: None,
     };
-
+    trace!("Creating swap input: {:?}", swap_input);
 
     let quote = executor.quote(&swap_input).await?;
-    println!("available quote: {:?}", quote.market);
+    trace!("Quote: {:?}", quote);
 
     let wallet = solana_sdk::signature::Keypair::from_base58_string(&env::var("SOLFLARE_WALLET")?);
-    
+    trace!("Wallet loaded: {:?}", wallet.pubkey());   
 
     let mut transaction = executor
         .swap_transaction(wallet.pubkey(), quote, None)
         .await?;
+    trace!("Transaction created: {:?}", transaction.message);
 
     let recent_blockhash = client.get_latest_blockhash().await?;
     transaction.message.set_recent_blockhash(recent_blockhash);
 
     let final_tx = VersionedTransaction::try_new(transaction.message,&[&wallet])?;
     client.send_and_confirm_transaction(&final_tx).await?;
-    println!("Transaction sent successfully: {:?}", final_tx.signatures);
     Ok(()) 
 
 }
